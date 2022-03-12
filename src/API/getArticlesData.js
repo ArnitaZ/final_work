49061bf14b5ba9d401af625533b00274ff4ffc3b
Arnita <arnitucis@inbox.lv>
2022-03-12 20:19:01 +0200

    API
    Creating data list.

---
+++ src/API/getArticlesData.js
@@ -0,0 +1,105 @@
+function getArticlesData() {
+  return [
+    {
+      id: 1,
+      title: "Couples",
+      description:
+        "Now that you and your bae are engaged, it’s time to commemorate the momentous occasion with a romantic photoshoot. But where do you start? There are so many cheesy couples' photos and countless run-of-the-mill shots that it can be a challenge to snap a photo that is truly your own. But thankfully, you don't have to rely on those predictable poses and overdone locations. There are a handful of things you can do to make sure your photos turn out perfectly one-of-a-kind and feel right for you and your partner. Before you jump into your session, here's what you should know about engagement photoshoots. For starters: Location Whether you and your significant other are world travelers or would rather just Netflix and chill, location is everything for making your engagement photos unique. Your Favorite Spot: Have a go-to coffee shop where the barista knows both of your orders? Or a brewery where you meet for happy hour every week? Use these personal spots as inspiration for your engagement shoot to save the memories of these places for years to come.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "3 hour",
+      price: "100 EUR",
+      location: "Jūrmala",
+    },
+    {
+      id: 2,
+      title: "Family",
+      description:
+        "A lot of people I talk to, say that they’d love to get some great images of their families, but they’re worried about what it will be like. They say they don’t know how to pose, worry that the kids will misbehave and feel like the images they have in their head aren’t possible for their family. Your family photo shoot should be a fun experience in itself. It’s not only an investment of your money but also of your time and let’s face it – who’s got either time or money they can afford to waste? Whether you want the photographs for your own wall or as gifts for the rest of the family, whether your children are small, teens or even furry – the experience should be something remembered with fondness and smiles.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "3 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 3,
+      title: "Wedding 1",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite. You can choose from 1 hour photosession to capture your wedding day all day long. This particular option is 2 hour long which includes ceremony, couple photogrphy and guest  ",
+      coverImg: "/coverImages/thailand.png",
+      duration: "2 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 4,
+      title: "Wedding 2",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "4 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 5,
+      title: "Wedding 3",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "6 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 6,
+      title: "Wedding 4",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "8 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 7,
+      title: "Wedding 5",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "10 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 8,
+      title: "Engagement",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "3 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 9,
+      title: "Wedding",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "3 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+    {
+      id: 10,
+      title: "Wedding",
+      description:
+        "All brides know that wedding photos are one of the most important parts of the wedding day, so coming up with a must-have wedding photo list should be high on your wedding planning priorities. These are the memories you'll cherish forever and even look back on the very next day (It goes by in a flash, they all say). That's why you want to make sure your photographer nails every photo-op, starting with the getting ready photos all the way through to your (sparkler) exit. Step one to checking off this to-do list is to hire a wedding photographer you're excited about—and put your trust in the professional. You are hiring just that—a professional—who should know what they're doing, after all. They'll have an idea of the must-have wedding shots, but even so, it's nice to have some general knowledge of wedding photography yourself. As your 101 guide, we put together a wedding photo list of the must-have moments to capture—from the pretty detailed shot of your wedding day jewels to the table settings and invitation suite.",
+      coverImg: "/coverImages/thailand.png",
+      duration: "3 hour",
+      price: "100 EUR",
+      location: "Latvia",
+    },
+  ];
+}
+export default getArticlesData;
